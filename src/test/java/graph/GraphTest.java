aed51646e30016304692d3efc0aa5f59d131fb70
hassanAhmed310 <h01552405179@gmail.com>
2023-05-02 11:25:25 +0200

    Adding tests by hassan

--- src/test/java/graph/GraphTest.java
+++ src/test/java/graph/GraphTest.java
@@ -11,9 +11,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class GraphTest {
     static Graph graph;
     static String test = "B:\\test.txt";
+    static String floydGraph = "B:\\floyd.txt";
     static String cycleGraph = "B:\\cycle.txt";
     static int[] costs;
     static int[] parents;
+    static int[][] warshallCosts;
+    static int[][] predecessors;
     long startTime, endTime;
     @BeforeAll
     static void init(){
@@ -27,6 +30,8 @@ class GraphTest {
     void initializeCostsAndParentsArrays(){
         costs = new int[8];
         parents = new int[8];
+        predecessors = new int[4][4];
+        warshallCosts = new int[4][4];
         for (int i=0;i<8;i++){
             costs[i] = Integer.MAX_VALUE;
             parents[i] = -1;
@@ -58,5 +63,28 @@ class GraphTest {
         var cost = new int[4];
         var parent = new int[4];
         assertFalse(cycle.bellmanFord(0,cost,parent));
+    }
+    @Test
+    void testFloydWarshall(){
+        var floyd = new Graph(floydGraph);
+        floyd.floydWarshall(warshallCosts,predecessors);
+        int[][] expectedCosts = {
+                {0, 3, 7, 5},
+                {2, 0, 6, 4},
+                {3, 1, 0, 5},
+                {5, 3, 2, 0}
+        };
+        int[][] expectedParents = {
+                {-1, 0, 3, 0},
+                {1, -1, 3, 1},
+                {1, 2, -1, 1},
+                {1, 2, 3, -1}
+        };
+        for(int i=0;i<4;i++){
+            for (int j=0;j<4;j++){
+                assertEquals(expectedCosts[i][j],warshallCosts[i][j]);
+                assertEquals(expectedParents[i][j],predecessors[i][j]);
+            }
+        }
     }
 }
